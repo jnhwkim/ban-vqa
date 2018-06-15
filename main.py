79d875e912b8058c9b85a172b7da198ec919d27c
Jin-Hwa Kim <jnhwkim@gmail.com>
2018-06-15 14:16:04 +0900

    Update seed to reproduce

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def parse_args():
     parser.add_argument('--input', type=str, default=None)
     parser.add_argument('--output', type=str, default='saved_models/ban')
     parser.add_argument('--batch_size', type=int, default=256)
-    parser.add_argument('--seed', type=int, default=1111, help='random seed')
+    parser.add_argument('--seed', type=int, default=1204, help='random seed')
     args = parser.parse_args()
     return args
 
