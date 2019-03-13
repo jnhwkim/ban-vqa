e1bd40bc779f8ce77ae88b20a22c83b90030f27f
Jin-Hwa Kim <jnhwkim@gmail.com>
2019-03-13 14:49:05 +0900

    Update download.sh
    The location of the source is updated. Please refer to the [Download page](https://visualqa.org/download.html).

--- tools/download.sh
+++ tools/download.sh
@@ -9,24 +9,24 @@ unzip data/glove.6B.zip -d data/glove
 rm data/glove.6B.zip
 
 # Questions
-wget -P data http://visualqa.org/data/mscoco/vqa/v2_Questions_Train_mscoco.zip
+wget -P data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip
 unzip data/v2_Questions_Train_mscoco.zip -d data
 rm data/v2_Questions_Train_mscoco.zip
 
-wget -P data http://visualqa.org/data/mscoco/vqa/v2_Questions_Val_mscoco.zip
+wget -P data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip
 unzip data/v2_Questions_Val_mscoco.zip -d data
 rm data/v2_Questions_Val_mscoco.zip
 
-wget -P data http://visualqa.org/data/mscoco/vqa/v2_Questions_Test_mscoco.zip
+wget -P data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Test_mscoco.zip
 unzip data/v2_Questions_Test_mscoco.zip -d data
 rm data/v2_Questions_Test_mscoco.zip
 
 # Annotations
-wget -P data http://visualqa.org/data/mscoco/vqa/v2_Annotations_Train_mscoco.zip
+wget -P data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip
 unzip data/v2_Annotations_Train_mscoco.zip -d data
 rm data/v2_Annotations_Train_mscoco.zip
 
-wget -P data http://visualqa.org/data/mscoco/vqa/v2_Annotations_Val_mscoco.zip
+wget -P data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip
 unzip data/v2_Annotations_Val_mscoco.zip -d data
 rm data/v2_Annotations_Val_mscoco.zip
 
